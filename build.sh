eaa6480f712406719d2c34901e0bbbb4da898d75
kirill <Petrovich_1811@mail.ru>
2017-09-10 18:58:41 +0300

    обновление скрипты сборки (для интела)

--- build.sh
+++ build.sh
@@ -1,20 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
-Build () {
+Build() {
+    C_COMPILER="gcc"
+    CXX_COMPILER="g++"
+
     root_dir="$( pwd )"
-    mkdir build
+    if [ ! -d build ]; then
+        mkdir build
+    fi
     cd build
     echo $root_dir
-    cmake -DCMAKE_BUILD_TYPE=RELEASE $root_dir
+    if [ "$1" = "intel" ]; then
+      source /opt/intel/bin/iccvars.sh intel64
+      C_COMPILER="icc"
+      CXX_COMPILER="icpc"
+    fi
+    cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER $root_dir
     make -j2
+    cd $root_dir
 }
 
 Clear() {
-  if [ -d build ]
-    then
+  if [ -d build ]; then
     rm -rf ./build
   fi
 }
 
-Clear
+if [ "$2"="clear" ]; then
+    Clear
+fi
+
 Build
