d825dee7805a9a7d7bcfefd44ab528809af61ac2
kirill <Petrovich_1811@mail.ru>
2017-10-01 02:07:30 +0300

    очищение кэша + degug/release

--- build.sh
+++ build.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 COMPILER=$1
+BUILD=$2
 
 Build() {
     C_COMPILER="gcc"
     CXX_COMPILER="g++"
-
+    TYPE_BUILD=Release
     root_dir="$( pwd )"
     if [ ! -d _build ]; then
         mkdir _build
     fi
     cd _build
+    if [ -f "CMakeCache.txt" ]; then
+        rm "CMakeCache.txt"
+    fi
     echo $root_dir
     if [ "$COMPILER" = "intel" ]; then
       source /opt/intel/bin/iccvars.sh intel64
       C_COMPILER="icc"
       CXX_COMPILER="icpc"
     fi
+    if [ "$BUILD" = "debug" ]; then
+        TYPE_BUILD=Debug
+    fi
     echo "COMPILER = $C_COMPILER"
-    cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER $root_dir
+    echo "TYPE_BUILD = $TYPE_BUILD"
+    cmake -DCMAKE_BUILD_TYPE=$TYPE_BUILD -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER $root_dir
     make -j2
     cd $root_dir
 }
@@ -29,7 +37,7 @@ Clear() {
   fi
 }
 
-if [ "$2" = "clear" ]; then
+if [ "$3" = "clear" ]; then
     echo "run clear directory"
     Clear
 #    echo "finish clear directory"
